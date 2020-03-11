ae0a35517dbfb72c5afb852482ce733ca8b1f1ca
indiana danaher <indiana.danaher@gmail.com>
2020-03-11 19:15:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,26 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0 
+  while counter < array.length do 
+    if array[counter] == value_to_find
+      return counter
+    end
+  counter += 1
+  end
+ # Add your solution here
 end
 
 def find_max_value(array)
+  counter = 0 
+  while counter < (array.length - 1) do
+    if array[counter] >= array[counter + 1]
+      max = array[counter] 
+    end
+    if array[counter] < array[counter + 1]
+      max = array[counter + 1] 
+    end
+  return max
+  end
+  
   # Add your solution here
 end
 
