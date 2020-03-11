08aad6a69a601ada870a6d3d1a8f2b5ab634f374
indiana danaher <indiana.danaher@gmail.com>
2020-03-11 19:24:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,8 +10,18 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-
-def find_min_value(array)
   
+def find_min_value(array)
+    min = array[0]
+  array.each do |item|
+    if(item < min)
+      min = item
+    end
+  end
+  return (max)
+  
+  # Add your solution here
+end
+
   # Add your solution here
 end
